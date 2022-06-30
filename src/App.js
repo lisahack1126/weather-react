627834bd491ff676bf39892b725c2120ab2fa41c
lisahack1126 <lisa.hack1126@gmail.com>
2022-06-30 11:00:18 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Weather from "./Weather";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App">
+        <h1>NPM</h1>
+        <Weather city="Louisville" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Weather.js
@@ -0,0 +1,19 @@
+import React from "react";
+import axios from "axios";
+import { Audio } from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${Math.round(
+        response.data.main.temp
+      )}°F`
+    );
+  }
+
+  let apiKey = "07d2964c500ffbfcd2b577b291d089bd";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=imperial`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return <Audio height="100" width="100" color="grey" ariaLabel="loading" />;
+}
